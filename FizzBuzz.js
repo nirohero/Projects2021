864d552af5ca97eba0e6606fc7da9197f47a94c5
Nirojan Kalaimannan <nirokal92@gmail.com>
2021-09-30 16:27:38 -0700

    Add files via upload
    FizzBuzz in Javascript

---
+++ FizzBuzz.js
@@ -0,0 +1,14 @@
+const n = 15;
+
+for( let i = 1;i <= n ; i++){
+    if (n%3==0 && n%5 == 0)
+        console.log("FizzBuzz");
+    else if (n%5==0)
+        console.log("Fizz");
+    
+    else if (n%3==0)
+        console.log("Buzz");
+    else
+        console.log(n);
+}
+
