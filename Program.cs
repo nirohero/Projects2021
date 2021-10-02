382233e9385ff558d1f5a1c055c7ec338d4764ce
Nirojan Kalaimannan <nirokal92@gmail.com>
2021-10-02 11:31:23 -0700

    Add files via upload
    C#
    Displaying sum of all elements of an array

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr1 = {1,3,5};//creating an interger array
+            int sum = 0;
+
+            for(int i =0; i <arr1.Length;i++){
+
+                sum = sum+arr1[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
