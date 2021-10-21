6368efe493e8e9463d4b3e1395ba2df8a0009630
Nirojan Kalaimannan <nirokal92@gmail.com>
2021-10-21 17:28:35 -0400

    Update findArrPair.js
    added hash-map method in O(1) time.

--- findArrPair.js
+++ findArrPair.js
@@ -5,14 +5,23 @@ let target = 3;
 findPair(arr,target);
 
 
-function findPair(arr1,target){
-
+function findPair(arr1,target){//brute force method
     for(let i=0; i<arr1.length;i++){
         for(let j=i+1; j<arr1.length;i++){
-            if (arr1[i]+arr1[j]==target){
+            if (arr1[i]+arr1[j]==target){//checks to see if the two values's sum is equal to the target
              console.log(arr[i], arr[j]);
              return;
         	}
         }
     }
 }
+function  findPair2(arr1, target){
+    //create empty hashmap
+        let hashmap = new Map();
+        for(let i = 0;i<arr1.length;i++){
+        if(hashmap.has(target - arr1[i])){
+            console.log(arr1[hashmap.get(target-arr1[i])], arr1[i]);
+            return;
+        }
+        }
+    }
